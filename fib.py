525eba583264cac5041cd48b5440c5812ef31f52
Marcell Himawan <mhimawan@hiya.com>
2019-09-18 13:41:46 -0700

    added more fib variants

--- fib.py
+++ fib.py
@@ -5,4 +5,22 @@ while a < 1000:
     print(a, end=',')
     a, b = b, a+b
 
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
+fib(2000)
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n: 
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+fVal = fib2(30000)
+print(fVal)
